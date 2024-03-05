66f760cdb945cbb5d73f67d071a05ba3b1c5f231
enrique <casillasenrique@live.com>
2024-03-05 00:58:07 -0500

    Remove ../ checking

--- static/global.js
+++ static/global.js
@@ -18,12 +18,10 @@ const pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
